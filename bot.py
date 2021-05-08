4fb33c925465c6e500b9c8665889db8fee48bb9f
Lozerd <dmisha29411@gmail.com>
2021-05-08 04:38:03 +0300

    ver 1.1 "/" converted to buttons

--- bot.py
+++ bot.py
@@ -19,51 +19,60 @@ class InlineButtons:
 
 # Reply Chat Buttons
 class ReplyButtons:
-    btn1 = types.KeyboardButton("Github")
-    btn2 = types.KeyboardButton("Pyrogram")
-    btn3 = types.KeyboardButton("Start")
-    btn4 = types.KeyboardButton("Subscribe")
-    btn5 = types.KeyboardButton("Unsubscribe")
+    btn1 = types.KeyboardButton("/Github")
+    btn2 = types.KeyboardButton("/Pyrogram")
+    btn3 = types.KeyboardButton("/Start")
+    btn4 = types.KeyboardButton("/Subscribe")
+    btn5 = types.KeyboardButton("/Unsubscribe")
     restart = types.KeyboardButton("Restart")
+
 
 @bot.message_handler(content_types=['new_chat_members'])
 def initial(message):
     bot.reply_to(message, "Список команд:\n\t/start\n\t/menu\n\t/subscribe\n\t/unsubscribe")
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, f"Добро пожаловать {message.from_user.first_name}!\n"
-                                      f"Ваш никнейм: {message.from_user.username}\n"
-                                      f"Ваш ID: {message.from_user.id}")
-
-
-@bot.message_handler(commands=['menu'])
-def menu(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn = ReplyButtons()
-    markup.add(btn.btn1, btn.btn2, btn.restart)
-    bot.send_message(message.chat.id, "Это главное меню, выберите опцию", reply_markup=markup)
-
-
 @bot.message_handler(content_types=['text'])
 def msg_parser(message):
     proccessed_message = message.text.strip().lower()
 
-    if proccessed_message == "github":
+    if proccessed_message == '/menu':
+        markup = types.ReplyKeyboardMarkup()
+        btn = ReplyButtons()
+        markup.add(btn.btn1, btn.btn2, btn.btn3, btn.btn4, btn.btn5, btn.restart)
+        end_message = "Это главное меню, выберите опцию"
+    elif proccessed_message == "/github":
         markup = types.InlineKeyboardMarkup()
         btn = InlineButtons()
         markup.add(btn.btn1)
         end_message = "Lozerd's Github"
-    elif proccessed_message == 'pyrogram':
+    elif proccessed_message == '/pyrogram':
         markup = types.InlineKeyboardMarkup()
         btn = InlineButtons()
         markup.add(btn.btn2)
         end_message = "Pyrogram Docs"
-    elif proccessed_message == 'start':
+    elif proccessed_message == '/start':
         return bot.send_message(message.chat.id, f"Добро пожаловать {message.from_user.first_name}!\n"
-                                          f"Ваш никнейм: {message.from_user.username}\n"
-                                          f"Ваш ID: {message.from_user.id}")
+                                                 f"Ваш никнейм: {message.from_user.username}\n"
+                                                 f"Ваш ID: {message.from_user.id}")
+    elif proccessed_message == '/subscribe':
+        if not db.subscriber_exists(message.from_user.id):
+            db.add_subscriber(message.from_user.id)
+            db.connection.commit()
+        else:
+            db.update_subscription(message.from_user.id, True)
+            db.connection.commit()
+
+        return bot.send_message(message.chat.id, "Вы успешно подписаны")
+    elif proccessed_message == '/unsubscribe':
+        if not db.subscriber_exists(message.from_user.id):
+            db.add_subscriber(message.from_user.id, False)
+            db.connection.commit()
+            return bot.send_message(message.chat.id, "Вы итак не подписаны")
+        else:
+            db.update_subscription(message.from_user.id, False)
+            db.connection.commit()
+        return bot.send_message(message.chat.id, "Вы успешно отписались")
     else:
         markup = types.ReplyKeyboardMarkup()
         btn = ReplyButtons()
@@ -72,31 +81,40 @@ def msg_parser(message):
     bot.send_message(message.chat.id, end_message, reply_markup=markup)
 
 
-@bot.message_handler(commands=['subscribe'])
-def subscribe(message):
-    if not db.subscriber_exists(message.from_user.id):
-        db.add_subscriber(message.from_user.id)
-        db.connection.commit()
-    else:
-        db.update_subscription(message.from_user.id, True)
-        db.connection.commit()
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     bot.send_message(message.chat.id, f"Добро пожаловать {message.from_user.first_name}!\n"
+#                                       f"Ваш никнейм: {message.from_user.username}\n"
+#                                       f"Ваш ID: {message.from_user.id}")
+# @bot.message_handler(commands=['menu'])
+# def menu(message):
+#     markup = types.ReplyKeyboardMarkup()
+#     btn = ReplyButtons()
+#     markup.add(btn.btn1, btn.btn2, btn.btn3, btn.btn4, btn.btn5, btn.restart)
+#     bot.send_message(message.chat.id, "Это главное меню, выберите опцию", reply_markup=markup)
+# @bot.message_handler(commands=['subscribe'])
+# def subscribe(message):
+#     if not db.subscriber_exists(message.from_user.id):
+#         db.add_subscriber(message.from_user.id)
+#         db.connection.commit()
+#     else:
+#         db.update_subscription(message.from_user.id, True)
+#         db.connection.commit()
+#
+#     return bot.send_message(message.chat.id, "Вы успешно подписаны")
 
-    return bot.send_message(message.chat.id, "Вы успешно подписаны")
-
-
-@bot.message_handler(commands=['unsubscribe'])
-def unsubscribe(message):
-    if not db.subscriber_exists(message.from_user.id):
-        db.add_subscriber(message.from_user.id, False)
-        db.connection.commit()
-        bot.send_message(message.chat.id, "Вы итак не подписаны")
-    else:
-        db.update_subscription(message.from_user.id, False)
-        db.connection.commit()
-        bot.send_message(message.chat.id, "Вы успешно отписались")
-
-
-''' ECHO COMMAND'''
+#
+# @bot.message_handler(commands=['unsubscribe'])
+# def unsubscribe(message):
+#     if not db.subscriber_exists(message.from_user.id):
+#         db.add_subscriber(message.from_user.id, False)
+#         db.connection.commit()
+#         bot.send_message(message.chat.id, "Вы итак не подписаны")
+#     else:
+#         db.update_subscription(message.from_user.id, False)
+#         db.connection.commit()
+#         bot.send_message(message.chat.id, "Вы успешно отписались")
+#
 # @bot.message_handler(content_types=['text'])
 # def body(message):
 #     if message.reply_to_message:
